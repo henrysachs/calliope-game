0987d37dd7ccfe67b29f817ed5e19c7e2d5ffffc
Henry Sachs <henrysachs@gmail.com>
2021-07-29 20:32:53 +0200

    play sound on touch

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let sprite = game.createSprite(2, 2)
+let sprite2 = game.createSprite(0, 2)
 basic.forever(function () {
-	
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+    if (sprite.isTouching(sprite2)) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
 })
